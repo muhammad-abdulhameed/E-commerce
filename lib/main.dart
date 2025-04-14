aad140d1722953e402066d939ea8711a1e7916d7
Muhammad <muhammadabdelhamed0@gmail.com>
2025-04-14 18:28:32 +0200

    +app  design

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:my_ecommerce/core/routes_manager/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routes_manager/route_generator.dart';
+import 'core/routes_manager/routes.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: child,
+        onGenerateRoute: RouteGenerator.getRoute,
+        initialRoute: Routes.signInRoute,
+      ),
+    );
+  }
+}
